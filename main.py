d705f707fbe4e4ecfea55cd315ff8910369ffa14
BillGamess <167017829+BillGamess@users.noreply.github.com>
2024-09-11 21:42:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ twitch_miner = TwitchChannelPointsMiner(
             disable_notification=True,                                              # Revoke the notification (sound/vibration)
         ),
         discord=Discord(
-            webhook_api="https://discord.com/api/webhooks/1282288046007652493/GJEch-jZGsr7P26OP1ORmXvUdJewiVFNDJao1oA5QVnP-5Lkd1rsx4eXRNxp-oCFbb1V",  # Discord Webhook URL
+            webhook_api="https://discord.com/api/webhooks/1283452509045784657/qXGzRQgeVFygLki2Ln6KDGm_NuSU6mNqdz93t4qpRLny8BBeHKd1FRPieG9TVaoO6yxN",  # Discord Webhook URL
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
                     Events.GAIN_FOR_RAID, Events.GAIN_FOR_CLAIM, Events.GAIN_FOR_WATCH, Events.GAIN_FOR_WATCH_STREAK, Events.JOIN_RAID, Events.BONUS_CLAIM, Events.BET_WIN, Events.BET_LOSE, Events.BET_REFUND, Events.BET_FILTERS, Events.BET_GENERAL, Events.BET_FAILED, Events.BET_START],                                  # Only these events will be sent to the chat
         ),
